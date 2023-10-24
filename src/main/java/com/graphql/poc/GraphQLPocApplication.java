a26bab821eee8d76546b1e7f69c28f2067064d51
imran-dev100 <111573254+imran-dev100@users.noreply.github.com>
2023-10-25 03:04:41 +0530

    Update GraphQLPocApplication.java

--- src/main/java/com/graphql/poc/GraphQLPocApplication.java
+++ src/main/java/com/graphql/poc/GraphQLPocApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * imran
+ * imran.ansari
  */
 @SpringBootApplication
 public class GraphQLPocApplication {
