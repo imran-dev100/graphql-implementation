8014cdfa9fe326e093af8bf0507ea81d9abaf1bb
imran-dev100 <111573254+imran-dev100@users.noreply.github.com>
2024-01-21 20:07:48 +0530

    Update EmployeeRepository.java

--- src/main/java/com/graphql/poc/repository/EmployeeRepository.java
+++ src/main/java/com/graphql/poc/repository/EmployeeRepository.java
@@ -5,9 +5,6 @@ import org.springframework.stereotype.Repository;
 
 import com.graphql.poc.entity.Employee;
 
-/**
- * imran.ansari
- */
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 }
