4043cdcee43d72b9e1d33f5883eacf20272f8a88
imran-dev100 <111573254+imran-dev100@users.noreply.github.com>
2024-01-23 20:58:14 +0530

    Update EmployeeController.java

--- src/main/java/com/graphql/poc/controller/EmployeeController.java
+++ src/main/java/com/graphql/poc/controller/EmployeeController.java
@@ -1,14 +1,12 @@
 package com.graphql.poc.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.graphql.poc.dto.EmployeeDto;
 import com.graphql.poc.entity.Employee;
 import com.graphql.poc.service.EmployeeService;
